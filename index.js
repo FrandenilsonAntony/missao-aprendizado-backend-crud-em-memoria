080c5f826c180c3416a34bd9302712c435cd319a
Frandenilson Antony <iabyantony88@gmail.com>
2024-05-12 05:05:28 +0000

    Endpoint `Read All [GET] /personagem implementado.

--- index.js
+++ index.js
@@ -5,4 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World!')
 })
 
+const lista = ['Java', 'Kotlin', 'Android']
+
+//EndPoint Read All [GET] /personagem
+app.get('/personagem', function(req, res) {
+  res.send(lista)
+})
 app.listen(3000)
